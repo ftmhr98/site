55146426b46d3798df89ee6e128ad469f232544f
fateme heidari <thiss@DESKTOP-ED2QTOO.(none)>
2020-11-25 09:53:54 +0330

    get and post
    Signed-off-by: fateme heidari <thiss@DESKTOP-ED2QTOO.(none)>

--- router.py
+++ router.py
@@ -5,16 +5,35 @@ import sqlalchemy
 
 
 app=FastAPI()
-db=[]
-class User (BaseModel):
-    name=str
-    pass=str
+tb_user=[]
+tb_coupon=[]
+tb_per=[]
 
+class User(BaseModel):
+    name=str
+    pas=str
+class Coupon(BaseModel):
+    name=str
+class Pers(BaseModel):
+    permis=str
 
 @app.get("/users")
-def get_users:
-    return db;
-@app.post("/users"):
+def get_users():
+    return tb_user
+@app.post("/users")
 def create_user(user:User):
-    return user;
+    return user
+@app.get("/Permission")
+def get_permis():
+    return tb_per
 
+@app.post("/Permission")
+def create_permis(permis:Pers):
+    return permis
+    
+@app.get("/Coupone")
+def get_coupon():
+    return tb_coupon
+app.post("/Coupone")
+def create_coupon(coupon:Coupon):
+    return coupon
