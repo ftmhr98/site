ec787706464d41a9d3b60a7723fd9c7ac9b6bdc9
thiss <fatemeheidari989@gmail.com>
2020-11-28 12:39:23 +0330

    query

--- reposintory/mysql.py
+++ reposintory/mysql.py
@@ -7,26 +7,35 @@ cnx = mysql.connector.connect(user='root', password='@27061378',
 cursor = cnx.cursor()
 
 
-def isfetch(query):
+def execute(query):
     result = query.fetchall()
     return result
 
 
 q1_user = ("SELECT  id, name , pass FROM users")
-isfetch(q1_user)
-q1_permis = ("SELECT * FROM permis")
-isfetch(q1_permis)
+execute(q1_user)
+
+q1_permis = ("SELECT * FROM Permission")
+execute(q1_permis)
+
 q1_usper = ("SELECT user_id,permission_id FROM User_permission")
-isfetch(q1_usper)
+execute(q1_usper)
+
 q1_coupone = ("SELECT coupon_name,user_coupon FROM Coupone")
-isfetch(q1_coupone)
+execute(q1_coupone)
+
 q2_user = ("INSERT INTO users (name , pass , email) VALUES (%s , %s , %s)")
-isfetch(q2_user)
-q2_permis = ("INSERT INTO permis (permis VALUES (%s))")
-isfetch(q2_permis)
+
+execute(q2_user)
+
+q2_permis = ("INSERT INTO Permission (permis )VALUES (%s)")
+execute(q2_permis)
+
 q2_coupone = ("INSERT INTO Coupone (coupon_name) VALUSE (%s)")
-isfetch(q2_coupone)
-q2_usper = ("SELECT users.name AS user_id , permis.id AS permission_id FROM User_permission INNER JOIN  users,permis  ")
-isfetch(q2_usper)
-q3_coupone = ("SELECT users.id AS user_coupon FROM users INNER JOIN users")
-isfetch(q3_coupone)
+execute(q2_coupone)
+
+q2_usper = ("INSERT INTO User_permission (user_id,permission_id) VALUES  (%s,%s)")
+execute(q2_usper)
+
+q3_coupone = ("INSERT INTO Coupone (coupon_name,user_coupon) VALUES  (%s,%S)")
+execute(q3_coupone)
--- reposintory/users.py
+++ reposintory/users.py
@@ -1,2 +1,16 @@
 from fastapi import FastAPI
+import database
 
+
+def get_user(users):
+    if users == database.user:
+        return users
+    else:
+        return "faild"
+
+
+def check_per(userper):
+    if userper == database.User_permission:
+        return userper
+    else:
+        return "faild"
